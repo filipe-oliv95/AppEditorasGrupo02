fb164675dcbe726defd19ded28c4c7d2f5dda4da
filipe-oliv95 <filipec.oliveira95@gmail.com>
2023-06-21 18:56:27 -0300

    correction of DataContext

--- AppLivrariaGrupo02/src/context/DataContext.js
+++ AppLivrariaGrupo02/src/context/DataContext.js
@@ -4,34 +4,40 @@ import jwt_decode from 'jwt-decode';
 //Contexto
 export const DataContext = createContext({});
 
+//Contexto Provider
 export const DataProvider = ({ children }) => {
-    const [dadosUsuarios, setDadosUsuarios] = useState('');
+    const [dadosUsuario, setDadosUsuario] = useState('');
 
-    // Provedor do contexto.
-    const armazenarDadosUsuario = (jwt) => {
+    //essa constante receberá como parâmero um jwt do backend e o decodificará
+    const armazenarDadosUsuario = ( jwt ) => {
         var jwtDecodificado = jwt_decode(jwt);
 
-        // .user = chave do json retornado pelo backend que contem os dados do ususario logado.
-        // A partir da linha a baixo, o conteudo do usuario sera a string:
-        // {\"id\":1,\"username\":\"user\",\"email\":\"user@mail.com\",\"roles\":[\"ROLE_USER\"]}
+        //.user - chave do json retornado pelo backend que contem os dados do usuario logado
+        //a partir da linha abaixo, o conteudo de usuário será a string:
+        //{\"id\":1,\"username\":\"user\",\"email\":\"user@mail.com\",\"roles\":[\"ROLE_USER\"]}
         var usuario = jwtDecodificado.user;
-        // Na linha de baixo a string json eh transformada em objeto javascript.
+
+        //a string json é transformada em um objeto Javascript
         usuario = JSON.parse(usuario);
 
-        setDadosUsuarios({
+        //setando os dados de usuário em um array
+        setDadosUsuario({
             id: usuario?.id,
             nome: usuario?.username,
             email: usuario?.email,
             token: jwt
         })
+        //o ponto de interrogação (?) é usado para caso o item seja nulo, retorna nulo e nao da erro
+        //para acessar os dados, apenas informar dadosUsuario.id
     }
 
     return (
+        //o contexto criado será retornado
         <DataContext.Provider value={{
-            dadosUsuarios,
+            dadosUsuario,
             armazenarDadosUsuario
         }}>
-            {children}
+            { children }
         </DataContext.Provider>
-    );
+    )
 }
